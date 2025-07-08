cb40adaaef36ad4c7cb64b11773d2756f70458be
Ivana Castillo <ivanaacastillo@gmail.com>
2025-07-07 22:22:57 -0400

    segundo comentario

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
-const inmuebles = [
+const estates = [
     { id: 1, address: "Calle 123", price: 150000, type: "Apartamento" },
     { id: 2, address: "Avenida 456", price: 230000, type: "Casa" },
     { id: 3, address: "Carrera 789", price: 120000, type: "Estudio" },
 ];
-app.get("/api/states", (req, res) => {
-    res.json(inmuebles);
+
+app.get("/api/estates", (req, res) => {
+    res.json(estates);
 });
+
 module.exports = app;
